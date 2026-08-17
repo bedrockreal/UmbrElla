427764c851e314747722334e988a15a6db2e1002
Ho, Felix <fhh25@ic.ac.uk>
2026-08-17 17:14:36 +0100

    perf: optimise change-skin.s to 10 ASM instructions by using r30 and rlwinm

--- src/change-skin.s
+++ src/change-skin.s
@@ -1,5 +1,5 @@
 #	Assembly code to change Neil/Ella's skin by pressing Y on the GBA CSS
-#	inject before 0x804fe378, only executed if player is at the GBA CSS and not changing character
+#	inject before 0x8040e378, only executed if player is at the GBA CSS and not changing character
 #	free registers: r0, r9, r10, r11
 
 # check for the right file
@@ -12,56 +12,49 @@ li		28, 0
 
 # main code: inject into 0x8040e378
 .4byte	0xc240e378
-.4byte	0x00000008
+.4byte	0x00000006
 
-#	check if (r26 & 0x800) != 0, if and only if the Y button is pressed.
-#	We want to activate skin change when the Y button is first pressed (don't change when holding). The previous value of r26 is stored in 0x801d1c24 (contains unused, BBA-related debug string), with value 64 62 61 6e (not interfering with 0x800)
+# r30 is the botton press (not hold) mask. check if (r30 & 0x800) != 0, iff Y is pressed
 
-# as of note on line 33, lis is moved here
+# as of note on line 27, lis is moved here
 lis		9, 0x804f
-
-lis		11, 0x801d
-lwz		10, 0x1c24(11)
-stw		26, 0x1c24(11)
-andc	10, 26, 10
-andi.	10, 10, 0x800
+andi.	0, 30, 0x800
 beq		YButtonNotPressed
 
-#	execution reaches here iff the Y button is indeed pressed.
+#	execution reaches here at the 1st frame Y button is indeed pressed.
 
 # set r0 = *(0x804f3850), the CSS cursor's column index (0-indexed), which corresponds to the GBA character index
 # note: we move the lis before the branch, so that the r9 is always 0x804f regardless of branch, and the replaced instruction need not be restored.
 
 lwz		0, 0x3850(9)
 
-#	since we forced loading GBA pair 0, mod r0 by 2
-andi.	0, 0, 1
-
 #	now load the tile -> ID array address (0x804ed86c) onto r9
-#	note:: we don't load this address directly onto r9, but notice that 0x804ed86c = 0x804f0000 - 0x2794, so we load 0x804f0000 onto r9 (already done), then add an offset of -0x2794 for every load and store
+.set	TILE_TO_ID_ARR_ADDR,	0x804ed86c
 
-# lis		9, 0x804f
-# lis		9, 0x804e
-# addi	9, 9, 0xd86c
+#	now we want to modify the word at TILE_TO_ID_ARR_ADDR + ((r0 & 0x1) << 2) (here we and it with 0x1 since we've forced loading from GBA slot 0
 
-#	the title -> ID array is an int array (each element is 4 bytes long). set r11 = r9 + r0 * 4 so that r11 points to the exact element to be modified
-slwi	0, 0, 2
-add		11, 9, 0
+#	r11 = (r0 & 1) << 2
 
-#	now, load the word pointed by r9 (call it x), set x := (x + 2) % 8, and store it back
-#	remark: as of line 28, we add the offset every time here.
+rlwinm	11, 0, 2, 29, 29
 
-lwz		10, -0x2794(11)
-addi	10, 10, 2
+# load the word onto r10, then update r11 to the address
+addi	11, 11, TILE_TO_ID_ARR_ADDR@l
+lwzux	10, 11, 9
+
+# set r10 := (r10 + 2) % 8
+addic	10, 10, 2
 andi.	10, 10, 7
-stw		10, -0x2794(11)
+
+# store r10
+stw		10, 0x0(11)
 
 
 YButtonNotPressed:
 
-#	no need to restore replaced instruction 'lis	r9, 0x804f'
+#	no need to restore replaced instruction 'lis r9, 0x804f'
 
 # end gecko code
+nop
 .4byte	0x00000000
 .4byte	0xe0000000
 .4byte	0x80008000
